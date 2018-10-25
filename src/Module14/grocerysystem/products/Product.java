53ea20aaf3d45ba2da0425f4f828f4053a471937
Nicolai Gram <nicolai@brnd.com>
2018-10-25 10:56:34 +0200

    Task 2

--- src/Module14/grocerysystem/products/Product.java
+++ src/Module14/grocerysystem/products/Product.java
@@ -1,32 +1,37 @@
 package Module14.grocerysystem.products;
 
+import Module14.grocerysystem.products.interfaces.Expirable;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public abstract class Product
-{
+public abstract class Product implements Expirable {
+
     private UUID serial;
     private String name;
     private double price;
-    
-    public Product(String name, double price)
-    {
+
+    public Product(String name, double price) {
         this.serial = UUID.randomUUID();
         this.name = name;
         this.price = price;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
-    public double getPrice()
-    {
+    public double getPrice() {
         return price;
     }
 
-    public UUID getSerial()
-    {
+    public UUID getSerial() {
         return serial;
     }
+
+    @Override
+    public boolean isExpired() {
+        throw new UnsupportedOperationException("”Product does not support this operation.");
+    }
+
 }
